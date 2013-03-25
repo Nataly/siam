d91625b60dfc0d10cee710fe5066ac632160cd02
Nataly Moros <nataly.moros1990@gmail.com>
2013-03-25 12:27:26 -0430

    Acordeón*

--- protected/views/pcuc/index.php
+++ protected/views/pcuc/index.php
@@ -1,19 +1,61 @@
-
-<?php 
-
-$this->widget('zii.widgets.jui.CJuiAccordion', array(
-    'panels'=>array(
-        'panel 1'=>'Content for panel 1',
-        'panel 2'=>'Content for panel 2',
-        
-    ),
-    'options'=>array(
-        'collapsible'=>true,
-        'active'=>1,
-    ),
-    'htmlOptions'=>array(
-        'style'=>'width:500px;'
-    ),
-));
-
-?>
+<div class="row-fluid">
+  
+  <div class="span6">
+  
+  
+     <div class="accordion" id="accordion1">
+        <div class="accordion-group">   
+        <div class="accordion-heading">
+          <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne">
+            Collapsible Group Item #1
+          </a>
+        </div>
+    
+        <div class="accordion-group" id="collapseOne">   
+        <div class="accordion-heading">
+          <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne2">
+            Collapsible Group Item #2
+          </a>
+        </div>
+        <div id="collapseOne2" class="accordion-body collapse">
+          <div class="accordion-inner">
+            <?php echo 'mmmmmm';?>
+          </div>
+        </div>    
+      </div> 
+            
+            
+            
+      </div>    
+      </div>
+      
+      <div class="accordion" id="accordion2">
+          
+        <div class="accordion-group">   
+        <div class="accordion-heading">
+          <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne2">
+            Collapsible Group Item #1
+          </a>
+        </div>
+            
+        <div id="collapseOne2" class="accordion-body collapse">
+          <div class="accordion-inner">
+            <?php echo 'mmmmmm';?>
+          </div>
+        </div>  
+            
+      </div>
+          
+      </div>
+          
+          
+    </div> 
+      
+      
+      
+    </div>
+    
+    
+   
+  </div>
+</div>
